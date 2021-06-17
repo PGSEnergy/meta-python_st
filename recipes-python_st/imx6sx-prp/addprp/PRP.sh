07a537d23f8b7ea12df097a0e6b109bf7dae55d2
Stefan12321 <denik1999@ukr.net>
2021-06-17 11:44:00 +0300

    fix in PRP.sh

--- recipes-python_st/imx6sx-prp/addprp/PRP.sh
+++ recipes-python_st/imx6sx-prp/addprp/PRP.sh
@@ -3,12 +3,12 @@
 sleep 1
 sleep 7; ifconfig prp1 10.207.97.122 &
 cd /prp/sw_stack_prp1-master/prp_pcap_tap_userspace/
-
+echo 1 > prp_conf.txt
 ret="$(cat prp_conf.txt)"
 
 
 com="./prp_pcap_tap_userspace eth0 eth1"
-echo 1 > prp_conf.txt
+
 
 if [ $ret != '0' ]; then
   $com
