6d754e94c3e429d1aa45b0d52955bf14bb125aba
Stefan12321 <denik1999@ukr.net>
2021-06-17 10:59:40 +0300

    Added background run for prp

--- recipes-python_st/imx6sx-prp/addprp/PRP.sh
+++ recipes-python_st/imx6sx-prp/addprp/PRP.sh
@@ -1,12 +1,19 @@
 #! /bin/sh
-# echo y > /sw_stack_prp1-master/prp_pcap_tap_userspace/prp_conf.txt
+(kill -TSTP $$; kill -CONT $$)
+sleep 1
+sleep 7; ifconfig prp1 10.207.97.122 &
 cd /prp/sw_stack_prp1-master/prp_pcap_tap_userspace/
-echo 1 > prp_conf.txt
+
 ret="$(cat prp_conf.txt)"
+
+
 com="./prp_pcap_tap_userspace eth0 eth1"
+echo 1 > prp_conf.txt
 
 if [ $ret != '0' ]; then
   $com
+  
+  
 else
   echo "Change prp_conf.txt value"  
 fi
